5dbd7a6f14e8960ecdec14302198895116c49e49
AVVKavvk <kumawatvipin066@gmail.com>
2023-06-01 11:53:23 +0530

    error

--- src/Routes/Signup.js
+++ src/Routes/Signup.js
@@ -29,7 +29,7 @@ function Signup() {
     if (number && isValidPhoneNumber(number)) {
       setError1("a");
     } else {
-      setError1("Phone Number Not Valid");
+      setError1("Phone Number Not Valid or please enter number without space ");
     }
     // console.log(err);
     // console.log(err1);
